a9b1403ac879ebc6999c82cea951985b49daf819
shristi2929 <95220373+shristi2929@users.noreply.github.com>
2023-08-22 23:53:40 +0530

    Add files via upload

---
+++ FIT.py
@@ -0,0 +1,5 @@
+# cook your dish here
+for t in range(int(input())):
+    x=int(input())
+    print(2*x*5)
+    
---
+++ TOP10.py
@@ -0,0 +1,13 @@
+# cook your dish here
+for t in range(int(input())): #input format contain integer T
+    x=int(input())  #input format for one integer X
+    if x==10:
+        print('YES')
+    elif x<=10:
+        print('YES')
+    #elif x>10:
+        #print('NO')
+    #elif x<=10:
+      #  print('NO')
+    else:
+        print('NO')
